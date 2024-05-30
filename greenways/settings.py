0817f1f072742e354b38d460b0e32b08d99adad9
dipchand <164710419+Dipchand20@users.noreply.github.com>
2024-05-30 13:47:32 +0100

    Update settings.py

--- greenways/settings.py
+++ greenways/settings.py
@@ -28,10 +28,7 @@ SECRET_KEY = 'django-insecure-ax6^id&mx_37&&seaziee_d!29dr!_#4h@h2z!z^0hlvocpz5g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ocassidydev-greenways-a1rwyegoqws.ws-eu104.gitpod.io',
-                '8000-ocassidydev-greenways-y13chb6125k.ws-eu104.gitpod.io', 
-                '8000-thecarboncrus-greenways-ezzjc7q7u39.ws-eu104.gitpod.io',
-                '127.0.0.1','localhost', 'ci-greenways-a76ef9e9c4a6.herokuapp.com','8000-ocassidydev-greenways-98htto6eqpj.ws-eu104.gitpod.io/']
+ALLOWED_HOSTS = ['*']
 
 
 
